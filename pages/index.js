645b7b758a7dd1132a7688e262af4165cec608a9
Duckies Mac <javi@Duckiess-iMac.local>
2021-02-06 00:40:24 -0800

    update - update home page header

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,9 @@ import {BaseLayout} from '../components/layouts/BaseLayout';
 import {LogoLeft} from '../components/shared/LogoHeaders';
 
 export default function Home() {
- console.log(process.env.NEXT_PUBLIC_WORKING)
   return (
     <BaseLayout >
-      <h1>{process.env.NEXT_PUBLIC_WORKING}</h1>
+      <h1>Categories</h1>
     </BaseLayout>
   )
 }
